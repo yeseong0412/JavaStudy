683480551de2ecce52f1a699ed5b3bee7c90b4e2
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-04-30 21:20:47 +0900

    JavaStudy

---
+++ src/MathTest.java
@@ -0,0 +1,7 @@
+public class MathTest {
+    public static void main(String[] args){
+        int value = Math.abs(-5);
+        System.out.println(value);
+        // Math m =new Math();
+    }
+}
---
+++ src/MyMath.java
@@ -0,0 +1,14 @@
+public class MyMath {
+    //아래 코드가 자동 생성된다. (기본 생성자가 없을경 컴파일시)
+    private MyMath(){
+
+    }
+    public static int abs(int x){
+        if(x<0){
+            return x*-1;
+        }
+        else{
+            return x;
+        }
+    }
+}
