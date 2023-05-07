591c7ca4ee4bbf1c9e0b1955b0bfdceb8033937f
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-05-07 20:57:25 +0900

    JavaStudy

---
+++ src/Exam01.java
@@ -0,0 +1,17 @@
+public class Exam01 {
+    public static void main(String[] args){
+        Parent p1 = new Parent();
+        System.out.println(p1.i);
+        p1.printI();
+        System.out.println("------------------");
+        Child c1 = new Child();
+        System.out.println(c1.i);
+        c1.printI();
+        System.out.println("------------------");
+        Parent p2 = new Child();
+        System.out.println(p2.i);
+        p2.printII();
+        p2.printI();
+
+    }
+}
