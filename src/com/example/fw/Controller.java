a6c49384e0b1e3ea15a8314fe2f3ca386d8cb43c
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-05-12 20:00:27 +0900

    JavaStudy 5/12

---
+++ src/com/example/fw/Controller.java
@@ -0,0 +1,24 @@
+package com.example.fw;
+
+/*
+com.example.fw.Controller 의 종유가 여러게더라.
+초기화 - 같은코드
+실행 - 다른코드?
+마무리 - 같은코드
+ */
+public abstract class Controller {
+    protected final void init(){
+        System.out.println("초기화 하는 코드");
+    }
+    protected final void close(){
+        System.out.println("마무리 하는 코드");
+    }
+    protected abstract void run();
+    //  내가 가진 메소드를 호출한다.
+    // 어떤 모양순서를 가지고 있다. 이런 메소드를 메소드 템플릿이라 한다.
+    public void execute(){
+        this.init(); //this 생략가능
+        this.run();
+        this.close();
+    }
+}
---
+++ src/com/example/myproject/FirstController.java
@@ -0,0 +1,14 @@
+package com.example.myproject;
+
+import com.example.fw.Controller;
+public class FirstController extends Controller {
+    @Override
+    public void run() {
+        System.out.println("별도로 동작하는 코드 11111");
+    }
+
+//    @Override
+//    protected void init() {
+//        System.out.println("내 마음대로 init");
+//    }
+}
