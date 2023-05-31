4e31a666ef2d2b35bf912512bef91c7f4ab96d90
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-05-31 22:03:06 +0900

    JavaStudy 5/31

---
+++ src/MenTo.java
@@ -0,0 +1,7 @@
+public class MenTo {
+    public static void main(String[] args) {
+        int num = 0;
+        String result = (num > 0) ? "양수입니다." : (num < 0) ? "음수입니다." : "0입니다.";
+        System.out.println(result);
+    }
+}
