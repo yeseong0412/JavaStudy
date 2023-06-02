93e7066291770b57629746f1e451f6d927f4ae1f
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-06-02 22:48:57 +0900

    JavaStudy 6/2

---
+++ src/Example.java
@@ -0,0 +1,28 @@
+//public class Example {
+//    public static void main(String[] args) {
+//        Person kgw = new Person("김건우", 18, "남");
+//        Person bsn = new Person("배성은", 17, "여");
+//        Person cmh = new Person("신민호", 17, "중성");
+//
+//        System.out.printf("%s %d %s\n", kgw.name, kgw.age, kgw.gender);
+//        System.out.printf("%s %d %s\n", bsn.name, bsn.age, bsn.gender);
+//        System.out.printf("%s %d %s\n", cmh.name, cmh.age, cmh.gender);
+//        System.out.println(kgw.eat());
+//    }
+//}
+//
+//public class Person {
+//    String name;
+//    int age;
+//    String gender;
+//
+//    public String eat() {
+//        return "먹다";
+//    }
+//
+//    public Person(String name, int age, String gender) {
+//        this.name = name;
+//        this.age = age;
+//        this.gender = gender;
+//    }
+//}
--- src/MenTo.java
+++ src/MenTo.java
@@ -1,6 +1,6 @@
 public class MenTo {
     public static void main(String[] args) {
-        int num = 0;
+        int num = -5;
         String result = (num > 0) ? "양수입니다." : (num < 0) ? "음수입니다." : "0입니다.";
         System.out.println(result);
     }
