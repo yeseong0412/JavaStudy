feacdee60137fb4834a1ee863a26f790eb7c2b00
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-06-04 01:36:27 +0900

    JavaStudy 6/4 (scanner)

--- src/AL.java
+++ src/AL.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 
 public class AL {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        if(a%2==0){
+        if (a % 2 == 0) {
             System.out.println("짝수");
-        }
-        else{
+        } else {
             System.out.println("홀수");
         }
     }
