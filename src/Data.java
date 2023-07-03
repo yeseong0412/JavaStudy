ad16af0ead1ea43b5a4c971e9dca15894b40146e
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-07-03 23:19:55 +0900

    7/3 (oop)

---
+++ src/Data.java
@@ -0,0 +1,17 @@
+public class Data { int x; }
+
+class Ex{
+    public static void main(String args[]) {
+        Data d = new Data();
+        d.x = 10;
+        System.out.println("main() : x = " + d.x);
+
+        change(d.x);
+        System.out.println("After change(d.x)");
+        System.out.println("main() : x = " + d.x);
+    }
+    static void change(int x) { //메서드의 매개변수가 기본형이면 > 읽기만 가능하다.
+        x = 1000;
+        System.out.println("change() : x = " + x);
+    }
+}
