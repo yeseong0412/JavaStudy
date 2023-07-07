b4318e3db3538ebe7f1b83887d125bd5bed45e27
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-07-07 22:15:13 +0900

    7/7 (java)

---
+++ src/Main2.java
@@ -0,0 +1,15 @@
+public class Main2 {
+    public static void main(String[] args) {
+        /* 기본 데이터 타입 */
+        int i = 1;
+        float f = 1.1f;
+        double d = 1.2;
+        boolean b = true;
+        char c = 'a';
+        System. out.println(i );
+        System. out.println(f );
+        System. out.println( d);
+        System. out.println(b );
+        System. out.println(c );
+    }
+}
