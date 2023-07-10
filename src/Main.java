372c1c7bd5302d8bf25c4c1623cbb33d80c1d823
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-07-10 09:50:05 +0900

    7/10 (AL)

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
-public class Main{
+public class Main {
     public static void main(String[] args){
-        VendingMachine.printVersion();
-        VendingMachine vm1 = new VendingMachine();
-        VendingMachine vm2 = new VendingMachine();
-        String product = vm1.pushProductButton(100);
-        System.out.println(product);
-        String product2 = vm2.pushProductButton(200);
-        System.out.println(product2);
+        System.out.println("'Hello'");
     }
 }
