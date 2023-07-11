1f602e0b91e447424c94f169c90eae4e256f0097
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-07-11 21:08:10 +0900

    7/11 (AL)

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-        System.out.println(a+" "+a+" "+a);
+        System.out.println(a+a);
     }
 }
