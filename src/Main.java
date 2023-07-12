a4fd742fa8cda57b80420c64f51168126a1e11f2
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-07-12 21:18:40 +0900

    7/12 (codeup 1032)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
-
 public class Main {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        String a = sc.nextLine();
-        System.out.println(a);
+        int i = Integer.valueOf(sc.next(), 8);
+        System.out.print(i);
     }
 }
