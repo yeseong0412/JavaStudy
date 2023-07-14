94517bec73188430ae5e483d4bd95e4efb4189d4
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-07-14 20:06:34 +0900

    7/14 (Kotlin)

---
+++ src/hel.kt
@@ -0,0 +1,10 @@
+fun main(args:Array<String>){
+        println("Enter name")
+        var name = readLine()
+        var age = 23
+        var job:String?
+        job = "Student"
+        println("name " + name)
+        println("age " + age)
+        println("job "+job)
+}
