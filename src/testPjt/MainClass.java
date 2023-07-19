62e4844544eb83d74e8e10e09a1cbd51cdd8432f
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-07-19 21:21:48 +0900

    7/19 (java oop grnd)

--- src/testPjt/MainClass.java
+++ src/testPjt/MainClass.java
@@ -2,15 +2,22 @@ package testPjt;
 
 public class MainClass {
     public static void main(String[] args){
-        char c = 'a';
-        int i = 10;
-        double d = 10.258;
-        boolean b = false;
-        String s = "Hello Java World!!";
-        System.out.println(c);
-        System.out.println(i);
-        System.out.println(d);
-        System.out.println(b);
-        System.out.println(s);
+        Grandeur myCar1 = new Grandeur();
+        myCar1.color = "red";
+        myCar1.gear = "auto";
+        myCar1.price = 100;
+
+        myCar1.run();
+        myCar1.stop();
+        myCar1.info();
+
+        Grandeur myCar2 = new Grandeur();
+        myCar2.color = "blue";
+        myCar2.gear = "auto";
+        myCar2.price = 100000;
+
+        myCar2.run();
+        myCar2.stop();
+        myCar2.info();
     }
 }
