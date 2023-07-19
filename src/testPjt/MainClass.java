f0be749efd969b6e5c2e1f24a3af8cda721d2993
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-07-19 21:05:02 +0900

    7/19 (java)

---
+++ src/testPjt/MainClass.java
@@ -0,0 +1,16 @@
+package testPjt;
+
+public class MainClass {
+    public static void main(String[] args){
+        char c = 'a';
+        int i = 10;
+        double d = 10.258;
+        boolean b = false;
+        String s = "Hello Java World!!";
+        System.out.println(c);
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(s);
+    }
+}
