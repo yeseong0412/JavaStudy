e300f3c9eb5356019c530cbce216c73739bfd2b9
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-07-20 20:17:19 +0900

    7/20 (this)

--- src/proj/MainClass.java
+++ src/proj/MainClass.java
@@ -2,14 +2,7 @@ package proj;
 
 public class MainClass {
     public static void main(String[] args) {
-        ObjectClass obj1 = new ObjectClass();
-        ObjectClass obj2 = new ObjectClass();
+        ObjectClass obj1 = new ObjectClass(10, 20);
         obj1.getInfo();
-
-        obj1 = null; //레퍼런스 x
-        //obj1.getInfo();
-
-        obj2.getInfo();
-
     }
 }
--- src/proj/ObjectClass.java
+++ src/proj/ObjectClass.java
@@ -1,7 +1,22 @@
 package proj;
 
+import javax.swing.text.html.ObjectView;
+
 public class ObjectClass {
+    public int x;
+    public int y;
+    public ObjectClass(String hello, int[] iArr) {
+        System.out.println("-- ObjectClass() --");
+
+        System.out.println("s --> " + hello);
+        System.out.println("iArr --> " + iArr);
+    }
+    public ObjectClass(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
     public void getInfo(){
-        System.out.println("--getInfo--");
+        System.out.println("x -> " + x);
+        System.out.println("y -> " + y);
     }
 }
