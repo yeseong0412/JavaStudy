4dcdb013d1b751426a370119edde5d74cec54ef1
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-07-22 21:29:24 +0900

    7/22 (extends)

---
+++ src/com/extend1/MainClass.java
@@ -0,0 +1,9 @@
+package com.extend1;
+
+public class MainClass {
+    public static void main(String[] args) {
+        ChildClass child1 = new ChildClass();
+        child1.childFun();
+        child1.parentFun();
+    }
+}
---
+++ src/com/extend1/ParentClass.java
@@ -0,0 +1,14 @@
+package com.extend1;
+
+public class ParentClass {
+    public ParentClass(){
+        System.out.println("ParentClass constructor");
+    }
+    public void parentFun(){
+        System.out.println("parentFun()");
+    }
+    private void privateFun(){
+        System.out.println("privateFun()");
+    }
+
+}
