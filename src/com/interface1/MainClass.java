8493d5e6560af967d484d829e42481dd9b1d365a
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-07-23 20:36:26 +0900

    7/23

--- src/com/interface1/MainClass.java
+++ src/com/interface1/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
     public static void main(String[] args) {
         InterfaceA ia = new InterfaceClass();
         InterfaceB ib = new InterfaceClass();
-
+        ib.funB();
         ia.funA();
         ib.funB();
     }
